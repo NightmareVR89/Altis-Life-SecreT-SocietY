adae7bc79f3555ec6d93a49217e75f6ea40e90c0
temako <nobsaibot@gmail.com>
2014-08-13 17:59:32 +0200

    Knopf versetzt

--- extDB-Build/Altis_Life.Altis/dialog/pInteraction.h
+++ extDB-Build/Altis_Life.Altis/dialog/pInteraction.h
@@ -139,6 +139,17 @@ class pInteraction_Menu
 			w = 0.24;
 			h = 0.038;
 		};
+		class ButtonEight : life_RscButtonMenu //Alkoholmesser
+		{
+			idc = 37457;
+			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
+			text = "";
+			sizeEx = 0.025;
+			x = BGX + 0.03;
+			y = BGY + 0.37;
+			w = 0.24;
+			h = 0.038;
+		};
 	};
 };
 
@@ -267,16 +278,6 @@ class vInteraction_Menu
 			w = 0.24;
 			h = 0.038;
 		};
-		class ButtonEight : life_RscButtonMenu
-		{
-			idc = 37457;
-			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
-			text = "";
-			sizeEx = 0.025;
-			x = BGX + 0.03;
-			y = BGY + 0.37;
-			w = 0.24;
-			h = 0.038;
-		};
+		
 	};
 };
